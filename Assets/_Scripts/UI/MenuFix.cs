ab1626f17076736261a03e66d60357b31f71c2a3
Arnaud Boulay <arnaud.boulay@epitech.eu>
2018-05-29 19:57:14 +0200

    Fix menu bug

---
+++ Assets/_Scripts/UI/MenuFix.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using VRTK;
+
+public class MenuFix : MonoBehaviour
+{
+    public GameObject EventSystem;
+    public VRTK_UIPointer PointerController;
+
+    void Start()
+    {
+        StartCoroutine(LateStart(1));
+    }
+
+    IEnumerator LateStart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        EventSystem.SetActive(true);
+        EventSystem.GetComponent<EventSystem>().enabled = false;
+        List<VRTK_UIPointer> pointers = EventSystem.GetComponent<VRTK_VRInputModule>().pointers;
+        if (pointers.Count == 0)
+            pointers.Add(PointerController);
+    }
+}
